6bbf392b4a9d93ef6f0129a7cef0214e08dd0530
Fahad Qazi <fahadqazi@gmail.com>
2023-07-31 14:18:00 +0100

    initial commit

---
+++ lua/plugins/glance.lua
@@ -0,0 +1,8 @@
+return {
+  "DNLHC/glance.nvim",
+  config = function()
+    require("glance").setup({
+      -- your configuration
+    })
+  end,
+}
---
+++ lua/plugins/themes.lua
@@ -0,0 +1,26 @@
+return {
+  { "jnurmine/Zenburn" },
+  { "rebelot/kanagawa.nvim" },
+  { "Shatur/neovim-ayu" },
+  { "mhartington/oceanic-next" },
+  { "sainnhe/gruvbox-material" },
+  { "EdenEast/nightfox.nvim" },
+  { "savq/melange-nvim" },
+  { "folke/tokyonight.nvim" },
+  { "altercation/vim-colors-solarized" },
+  { "morhetz/gruvbox" },
+  { "sainnhe/everforest" },
+  { "doums/darcula" },
+  { "lifepillar/vim-solarized8" },
+  { "nvimdev/oceanic-material" },
+  { "catppuccin/vim" },
+  "adibhanna/gruvdark.nvim",
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      -- colorscheme = "oceanicNext",
+      colorscheme = "gruvbox-material",
+    },
+  },
+}
